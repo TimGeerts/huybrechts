282eace5c6311aa5337cadfb5a373dc7956a99ad
Tim Geerts <timgeerts@Tims-MacBook-Air.local>
2021-04-11 16:00:13 +0200

    footer & google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'roboto': ['Roboto', 'ui-sans-serif', 'system-ui']
+    }
   },
   variants: {
     extend: {},
